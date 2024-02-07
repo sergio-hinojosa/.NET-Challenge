36376e4518789a8d01714cb1caeb90f8ff8e30f4
sergio-hinojosa <sergio.hinojosa@icloud.com>
2024-02-07 01:24:54 -0600

    Test MS Challenge

---
+++ ConsoleApp2/First Code.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
---
+++ ConsoleApp3/First and second line.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
